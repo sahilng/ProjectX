d4ed39270b4ed2a0db98ce0416324a4d682b15b3
sahilng <sahilng1997@gmail.com>
2014-04-21 21:40:35 -0400

    asd

--- kill.php
+++ kill.php
@@ -8,28 +8,28 @@
 require('keys.php');
 
 function kill($killer_id, $target_id){
-	$conn=mysqli_connect($mysql_server,$mysql_user,$mysql_password,$mysql_db);
+	//$con=mysqli_connect($mysql_server,$mysql_user,$mysql_password,$mysql_db);
 	
 	//first get target's target
-	$tt_query = mysqli_query($conn,'SELECT target from People where id='.$target_id);
+	$tt_query = mysqli_query($con,'SELECT target from People where id='.$target_id);
 	$tt_array = mysqli_fetch_array($tt_query);
 	var_dump($tt_array);
 	$targets_target = $tt_array[0];
 	
 	
 	//then set the target's target to 0
-	mysqli_query($conn, 'UPDATE People SET target=0 where id='.$target_id);
+	mysqli_query($con, 'UPDATE People SET target=0 where id='.$target_id);
 	echo "<br>target's target set to zero<br>";
 	//then set the killer's target to the target's old target
-	mysqli_query($conn, 'UPDATE People SET target='.$targets_target.' where id='.$killer_id);
+	mysqli_query($con, 'UPDATE People SET target='.$targets_target.' where id='.$killer_id);
 	echo "killer's target set to ".$targets_target."<br>";
 	//then set target's alive to no
-	mysqli_query($conn, 'UPDATE People SET alive=0 where id='.$target_id);
+	mysqli_query($con, 'UPDATE People SET alive=0 where id='.$target_id);
 	echo "target set to dead<br>";
 	//then increment killer's kills
-	mysqli_query($conn, 'UPDATE People SET kills = kills + 1 where id='.$killer_id);
+	mysqli_query($con, 'UPDATE People SET kills = kills + 1 where id='.$killer_id);
 	echo "killer's kills incremented<br>";
-	mysqli_close($conn);
+	//mysqli_close($con);
 }
 
 $killer = $_GET["killer"];
